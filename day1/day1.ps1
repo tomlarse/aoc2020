9d58663a6c492c4f79192eab1f61507930eb0e00
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2020-12-02 09:58:33 +0100

    day 1 complete

--- day1/day1.ps1
+++ day1/day1.ps1
@@ -16,3 +16,26 @@ function Invoke-Day1Task1 {
         $i++
     }
 }
+
+function Invoke-Day1Task2 {
+    param (
+        $entries
+    )
+    $i = 0
+    foreach ($firstentry in $entries) {
+        $j = 0
+        foreach ($secondentry in $entries) {
+            $k = 0
+            foreach ($thirdentry in $entries) {
+                if ($i -ne $j -and $i -ne $k) {
+                    if ($firstentry + $secondentry + $thirdentry -eq 2020) {
+                        return $firstentry * $secondentry * $thirdentry
+                    }
+                }
+                $k++
+            }
+            $j++
+        }
+        $i++
+    }
+}
--- day1/tests/day1tests.ps1
+++ day1/tests/day1tests.ps1
@@ -6,3 +6,9 @@ Describe "Day 1 Task 1" {
         Invoke-Day1Task1 -Entries @(1721, 979, 366, 299, 675, 1456) | Should -Be 514579
     }
 }
+
+Describe "Day 1 Task 2" {
+    It "Calculates example data correctly" {
+        Invoke-Day1Task2 -Entries @(1721, 979, 366, 299, 675, 1456) | Should -Be 241861950
+    }
+}
